d6840a2c4ced1e0e862ae07b878e8f6c1a937d8a
Ethan Pineda <joshuapineda66@gmail.com>
2022-12-21 11:22:48 -0600

    Fixing file pathing for picture of me

---
+++ build/precache-manifest.749a1298060084de7a199d50700b067b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a49700d045b0e700b3c53d43f553a04",
+    "url": "/Ethan/index.html"
+  },
+  {
+    "revision": "5ca2dacfe1eb2a21faad",
+    "url": "/Ethan/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "1d684d0b21cd8cabd402",
+    "url": "/Ethan/static/css/main.967d2986.chunk.css"
+  },
+  {
+    "revision": "5ca2dacfe1eb2a21faad",
+    "url": "/Ethan/static/js/2.9efc9183.chunk.js"
+  },
+  {
+    "revision": "1a042000e9301921a4141483110c357d",
+    "url": "/Ethan/static/js/2.9efc9183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a5952c791067e9b2a7",
+    "url": "/Ethan/static/js/3.79165e44.chunk.js"
+  },
+  {
+    "revision": "1d684d0b21cd8cabd402",
+    "url": "/Ethan/static/js/main.6f0930e8.chunk.js"
+  },
+  {
+    "revision": "2a7925844b6dc406ac49",
+    "url": "/Ethan/static/js/runtime-main.304f1243.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Ethan/static/media/logo.06e73328.svg"
+  }
+]);
