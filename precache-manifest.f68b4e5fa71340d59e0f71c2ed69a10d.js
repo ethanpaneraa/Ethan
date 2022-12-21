da6ab21a0affb4dc13e00d6c1cc8ba36d44e1593
Ethan Pineda <joshuapineda66@gmail.com>
2022-12-21 02:36:50 -0600

    Updates

---
+++ precache-manifest.f68b4e5fa71340d59e0f71c2ed69a10d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ab4a06e63461fa7715594fe61cdab1",
+    "url": "/Ethan/index.html"
+  },
+  {
+    "revision": "5ca2dacfe1eb2a21faad",
+    "url": "/Ethan/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "0ca184289d56d02548bc",
+    "url": "/Ethan/static/css/main.967d2986.chunk.css"
+  },
+  {
+    "revision": "5ca2dacfe1eb2a21faad",
+    "url": "/Ethan/static/js/2.9efc9183.chunk.js"
+  },
+  {
+    "revision": "1a042000e9301921a4141483110c357d",
+    "url": "/Ethan/static/js/2.9efc9183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a5952c791067e9b2a7",
+    "url": "/Ethan/static/js/3.79165e44.chunk.js"
+  },
+  {
+    "revision": "0ca184289d56d02548bc",
+    "url": "/Ethan/static/js/main.6e093a58.chunk.js"
+  },
+  {
+    "revision": "2a7925844b6dc406ac49",
+    "url": "/Ethan/static/js/runtime-main.304f1243.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Ethan/static/media/logo.06e73328.svg"
+  }
+]);
